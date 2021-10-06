12533e2838f292b367532b22fea4a31ce54d9097
IMMAMA AYOUB <you@example.com>
2021-10-06 17:37:16 -0400

    adding and updating python file

---
+++ main.py
@@ -0,0 +1,10 @@
+name = "Immama"
+eyeColor = "DarkBrown"
+height = 5.5
+HairColour = "black"
+
+
+print("=======================================================")
+print("My name is" + name + "and I have " + eyeColor + "eyes")
+print("I'm about" + str(height) + "tall and have" + HairColour + "hair.") 
+print("=======================================================")
