1b8bb74027add6f2c9d4d265007b30674555226e
IMMAMA AYOUB <you@example.com>
2021-10-20 17:29:28 -0400

    removing lives

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
 
-
+playerLives = 5
+compLives = 5
 
 choices = ["rock", "paper", "scissor"]
 
@@ -16,23 +17,36 @@ if (comp == player):
 	# tie - nothing else to compare, so it'll sxit
 	print("tie!!!! try agiain")
 
+
 elif (player == "rock"):
 	if(comp == "paper"):
 		print("you losee!!")
+		playerLives = playerLives - 1
 	else:
 		print("you win")
+		compLives = compLives - 1
 
 
 elif (player == "paper"):
 	if(comp == "scissor"):
 		print("you losee!!")
+		playerLives = playerLives - 1
 	else:
 		print("you win")
+		compLives = compLives - 1
+
 
 
 elif (player == "scissor"):
 	if(comp == "rock"):
 		print("you losee!!")
+		playerLives = playerLives - 1
 	else:
 		print("you win")
+		compLives = compLives - 1
+
+
+print("Player Life count: " + str(playerLives))
+print("Computer Life Count: " + str(compLives))
+
 
