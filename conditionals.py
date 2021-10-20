a0fc4b8fec67ea40db6a666201a424002073a946
IMMAMA AYOUB <you@example.com>
2021-10-20 17:20:11 -0400

    comparison logic working

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+tempr = int(input("Enter a number btw 0 and 120: "))
+
+
+if (tempr <= 4):
+    print("water is a solid")
+elif (tempr < 100):
+    print("water is a liquid")
+else:
+    print("water is a vapour")
+
+
--- main.py
+++ main.py
@@ -12,4 +12,27 @@ comp = choices[randint(0, 2)]
 print("player chose: " + player)
 print("computer chose: " + comp)
 
+if (comp == player):
+	# tie - nothing else to compare, so it'll sxit
+	print("tie!!!! try agiain")
+
+elif (player == "rock"):
+	if(comp == "paper"):
+		print("you losee!!")
+	else:
+		print("you win")
+
+
+elif (player == "paper"):
+	if(comp == "scissor"):
+		print("you losee!!")
+	else:
+		print("you win")
+
+
+elif (player == "scissor"):
+	if(comp == "rock"):
+		print("you losee!!")
+	else:
+		print("you win")
 
