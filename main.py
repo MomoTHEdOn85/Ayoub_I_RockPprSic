a091a149171a89a635eaabc9407b7461f34ec0ad
IMMAMA AYOUB <you@example.com>
2021-11-08 17:57:59 -0500

    loop working

--- main.py
+++ main.py
@@ -1,52 +1,66 @@
 from random import randint
 
-playerLives = 5
-compLives = 5
-
 choices = ["rock", "paper", "scissor"]
 
 # player will be the wepapon the player choose via input
-player = input("Choose your weapon: rock, paper, scissor: ")
+#Bolean values are True and False - you can use these to check state and then programming choicrs based on their value.
+player = False
 
-comp = choices[randint(0, 2)]
+#player = input("Choose your weapon: rock, paper, scissor: ")
 
-print("player chose: " + player)
-print("computer chose: " + comp)
+#comp = choices[randint(0, 2)]
 
-if (comp == player):
-	# tie - nothing else to compare, so it'll sxit
-	print("tie!!!! try agiain")
+#these lives need to decrement when a player loses a round
+playerLives = 5
+compLives = 5
 
+while player is False:
 
-elif (player == "rock"):
-	if(comp == "paper"):
-		print("you losee!!")
-		playerLives = playerLives - 1
-	else:
-		print("you win")
-		compLives = compLives - 1
+	player = input("Choose your weapon: rock, paper, scissor:")
 
+	comp = choices[randint(0, 2)]
 
-elif (player == "paper"):
-	if(comp == "scissor"):
-		print("you losee!!")
-		playerLives = playerLives - 1
-	else:
-		print("you win")
-		compLives = compLives - 1
+	print("player chose: " + player)
+	print("computer chose: " + comp)
 
 
-
-elif (player == "scissor"):
-	if(comp == "rock"):
-		print("you losee!!")
-		playerLives = playerLives - 1
-	else:
-		print("you win")
-		compLives = compLives - 1
+	if (comp == player):
+		# tie - nothing else to compare, so it'll sxit
+		print("tie!!!! try agiain")
 
 
-print("Player Life count: " + str(playerLives))
-print("Computer Life Count: " + str(compLives))
+	elif (player == "rock"):
+		if(comp == "paper"):
+			print("you losee!!")
+			playerLives = playerLives - 1
+		else:
+			print("you win")
+			compLives = compLives - 1
+
+
+	elif (player == "paper"):
+		if(comp == "scissor"):
+			print("you losee!!")
+			playerLives = playerLives - 1
+		else:
+			print("you win")
+			compLives = compLives - 1
+
+
+
+	elif (player == "scissor"):
+		if(comp == "rock"):
+			print("you losee!!")
+			playerLives = playerLives - 1
+		else:
+			print("you win")
+			compLives = compLives - 1
+
+
+	print("Player Life count: " + str(playerLives))
+	print("Computer Life Count: " + str(compLives))
+
+	player = False
+
 
 
